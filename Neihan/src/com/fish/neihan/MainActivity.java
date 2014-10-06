324281b17aed5a73464b38eba52fd0b0bcff126b
Zhang hao <fish@163.com>
2014-10-06 12:34:36 +0800

    主界面

--- Neihan/src/com/fish/neihan/MainActivity.java
+++ Neihan/src/com/fish/neihan/MainActivity.java
@@ -1,15 +1,54 @@
 package com.fish.neihan;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fish.neihan.fragments.HuoDongFragment;
+import com.fish.neihan.fragments.ImageListFragment;
+import com.fish.neihan.fragments.MyFragment;
+import com.fish.neihan.fragments.ReviewFragment;
+import com.fish.neihan.fragments.TextListFragment;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity implements
+		OnCheckedChangeListener {
+	private RadioGroup radioGroup;
+
+	private List<Fragment> fragments;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		radioGroup = (RadioGroup) findViewById(R.id.main_tab_bar);
+		radioGroup.setOnCheckedChangeListener(this);
+		
+
+		fragments = new ArrayList<Fragment>();
+		fragments.add(new TextListFragment());
+		fragments.add(new ImageListFragment());
+		fragments.add(new ReviewFragment());
+		fragments.add(new HuoDongFragment());
+		fragments.add(new MyFragment());
+
+		Fragment fragment = fragments.get(0);
+
+		FragmentManager manager = getSupportFragmentManager();
+		FragmentTransaction transaction = manager.beginTransaction();
+		transaction.replace(R.id.main_fragment, fragment);
+		transaction.commit();
+
 	}
 
 	@Override
@@ -17,6 +56,47 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onCheckedChanged(RadioGroup group, int checkedId) {
+		// TODO Auto-generated method stub
+		int childCount = radioGroup.getChildCount();
+		int checkIndex = 0;
+		RadioButton btn = null;
+		for (int i = 0; i < childCount; i++) {
+			btn = (RadioButton) radioGroup.getChildAt(i);
+			if (btn.isChecked()) {
+				checkIndex = i;
+				break;
+			}
+		}
+		Fragment fragment = fragments.get(checkIndex);
+
+		FragmentManager manager = getSupportFragmentManager();
+		FragmentTransaction transaction = manager.beginTransaction();
+		transaction.replace(R.id.main_fragment, fragment);
+		transaction.commit();
+		// switch (checkIndex) {
+		// case 0:
+		//
+		// break;
+		// case 1:
+		//
+		// break;
+		// case 2:
+		//
+		// break;
+		// case 3:
+		//
+		// break;
+		// case 4:
+		//
+		// break;
+		//
+		// default:
+		// break;
+		// }
 	}
 
 }
---
+++ Neihan/src/com/fish/neihan/fragments/HuoDongFragment.java
@@ -0,0 +1,46 @@
+package com.fish.neihan.fragments;
+
+import com.fish.neihan.R;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+/**
+ * 
+ * @author Squirrelfish
+ *
+ */
+public class HuoDongFragment extends Fragment {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_activity, container, false);
+		return view;
+	}
+	@Override
+	public void onDestroyView() {
+		// TODO Auto-generated method stub
+		super.onDestroyView();
+	}
+	
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+	
+	@Override
+	public void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+}
